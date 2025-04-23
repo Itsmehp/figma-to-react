2469641be59d9ad6baa34119425576b9f9eee6b8
Itsmehp <harshil.hpz@gmail.com>
2025-04-23 16:28:29 +0200

    CTA - Started

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,23 +1,24 @@
-import logoblack from "./logo-black.png";
+import logoblack from "./enlarged/logo-black.png";
 import black1 from "./black-green.svg";
 import black2 from "./black-white.svg";
-import hero from "./hero.png";
+import hero from "./enlarged/hero.png";
 import c1 from "./Companylogo-1.png";
 import c2 from "./Companylogo-2.png";
 import c3 from "./Companylogo-3.png";
 import c4 from "./Companylogo-4.png";
 import c5 from "./Companylogo-5.png";
 import c6 from "./Companylogo-6.png";
-import I1 from "./Illustration-1.png";
-import I2 from "./Illustration-2.png";
-import I3 from "./Illustration-3.png";
-import I4 from "./Illustration-4.png";
-import I5 from "./Illustration-5.png";
-import I6 from "./Illustration-6.png";
+import I1 from "./enlarged/Illustration-1.png";
+import I2 from "./enlarged/Illustration-2.png";
+import I3 from "./enlarged/Illustration-3.png";
+import I4 from "./enlarged/Illustration-4.png";
+import I5 from "./enlarged/Illustration-5.png";
+import I6 from "./enlarged/Illustration-6.png";
 import blackgreen from "./black-green.svg";
 import whiteblack from "./white-black.svg";
 import blackwhite from "./black-white.svg";
 import whitegreen from "./white-green.svg";
+import cta from "./enlarged/Illustration-ccta.png";
 
 export {
   hero,
@@ -40,4 +41,5 @@ export {
   whiteblack,
   whitegreen,
   blackwhite,
+  cta,
 };
--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import "./cta.css";
+import { cta } from "../../assets";
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="wrapper">
+      <div className="cta">
+        <div className="cta__description">
+          <h3>Let’s make things happen</h3>
+          <p>
+            Contact us today to learn more about how our digital marketing
+            services can help your business grow and succeed online.
+          </p>
+          <button className="cta__button">Book a Consultation</button>
+        </div>
+        <div className="cta__illustration">
+          <img src={cta} alt="call to action button" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
